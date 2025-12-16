6a34ad5406ce281f0d7c589f8161d03f671eff0e
Dishant1-git <Dishantkaushal456@gmail.com>
2025-12-16 19:14:10 +0530

    update

--- diytech.js
+++ diytech.js
@@ -59,7 +59,7 @@ async function main() {
   console.log("db connected");
   await mongoose.connect(process.env.MONGODB_URL);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
 }
 
 app.use("/", indexRouter);
